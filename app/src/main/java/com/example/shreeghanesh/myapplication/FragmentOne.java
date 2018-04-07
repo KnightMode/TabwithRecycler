cad83eb1e0cc0ab4e4a851905509a746d12f2fdc
sgbcoder <ghanesh.balaji1995@gmail.com>
2018-04-07 17:29:32 +0530

    First Commit

---
+++ app/src/main/java/com/example/shreeghanesh/myapplication/FragmentOne.java
@@ -0,0 +1,21 @@
+package com.example.shreeghanesh.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentOne extends Fragment {
+    View view;
+    public FragmentOne() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_one,container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/shreeghanesh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shreeghanesh/myapplication/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.shreeghanesh.myapplication;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private AppBarLayout appBarLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.main_tab);
+        viewPager = findViewById(R.id.view_pager);
+        appBarLayout = findViewById(R.id.app_bar);
+        ViewPagert viewPagert = new ViewPagert(getSupportFragmentManager());
+        viewPagert.addFragment(new FragmentOne(), "Tab-One");
+        viewPagert.addFragment(new FragmentTwo(), "Tab-two");
+        viewPager.setAdapter(viewPagert);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
